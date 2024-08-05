cf0a332948cb21fb4d8c5ea1fb2e3a51b69829c8
Shamsundar12316 <168196650+Shamsundar12316@users.noreply.github.com>
2024-08-06 02:09:22 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// famous quote
+let quote = "A person who never make a mistake never tried anything new";
+let author = "Albert Einstein";
+console.log(`${author} once said, ${quote}`);
